179c1004b75df94b41e948e436d2c0727a3f2bfa
Bermannatanael <bermanloi05@gmail.com>
2023-12-21 08:13:32 +0700

    first commit

---
+++ src/test/java/com/example/graphqltest/GraphqlTest/GraphqlTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphqltest.GraphqlTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
